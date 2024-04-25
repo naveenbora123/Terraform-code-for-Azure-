0eb06d65c6712de70d6a0a54750312db88d03ccf
naveenbora123 <68266357+naveenbora123@users.noreply.github.com>
2024-04-25 19:06:28 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 The following links provide the documentation for the new blocks used
 in this terraform configuration file
 
-1. azurerm_resource_group - https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
+1.azurerm_storage_account - https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account
 
 */
 
@@ -18,13 +18,22 @@ terraform {
 
 provider "azurerm" {
   subscription_id = "6912d7a0-bc28-459a-9407-33bbba641c07"
-  client_id       = "08d82452-5ee9-4b5b-96ea-aec56d9db182"
-  client_secret   = "oc38Q~D5P_jTpGSStayL3_3pT2Kqm2olimm2Ja_p"
-  tenant_id       = "70c0f6d9-7f3b-4425-a6b6-09b47643ec58"
-  features {}
+  tenant_id = "70c0f6d9-7f3b-4425-a6b6-09b47643ec58"
+  client_id = "fb3eb97c-8679-488e-a4b4-5fd90299b7a2"
+  client_secret = "38o8Q~MsAPo59FK.Zsq0eQImB-vW2wrJznAUqc9V"
+  features {}  
 }
 
 resource "azurerm_resource_group" "appgrp" {
   name     = "app-grp"
   location = "North Europe"
+}
+
+resource "azurerm_storage_account" "appstore566565637" {
+  name                     = "appstore566565637"
+  resource_group_name      = "app-grp"
+  location                 = "North Europe"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind = "StorageV2"
 }
